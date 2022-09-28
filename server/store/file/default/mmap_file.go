c8f4e74518b3e1d062940c7de4e03d5854f25ccf
allen <allenshaw19@gmail.com>
2022-09-29 00:29:51 +0800

    add n

--- server/store/file/default/mmap_file.go
+++ server/store/file/default/mmap_file.go
@@ -1,14 +1,16 @@
 package _default
 
 import (
-	"bytes"
 	"go.uber.org/atomic"
 	"os"
 	"syscall"
+	"unsafe"
 )
 
 const (
 	OsPageSize = 1024 * 4
+	defaultMaxFileSize = 1 << 30        // 假设文件最大为 1G
+	defaultMemMapSize = 128 * (1 << 20) // 假设映射的内存大小为 128M
 )
 
 var (
@@ -18,21 +20,26 @@ var (
 
 type MmapFile struct {
 	startPos       atomic.Int32
-	wrotePos       atomic.Int32
+	wrotePos       *atomic.Int32
 	committedPos   atomic.Int32
 	flushedPos     atomic.Int32
-	file           *os.File
 	fileSize       int
 	fileFromOffset int64
 	fileName       string
-	mappedByteBuff *bytes.Buffer
 	storeTimestamp atomic.Int64
+
+	// mmapfile
+	file           *os.File
+	data *[defaultMaxFileSize]byte
+	offset int
+	dataRef []byte
 }
 
 func NewMmapFile(fileName string, fileSize int) (*MmapFile, error) {
 	mf := &MmapFile{}
 	mf.fileName = fileName
 	mf.fileSize = fileSize
+	mf.wrotePos = atomic.NewInt32(0)
 	ok := false
 
 	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, os.ModePerm)
@@ -47,12 +54,14 @@ func NewMmapFile(fileName string, fileSize int) (*MmapFile, error) {
 	}()
 
 	mf.file = f
-	buff, err := syscall.Mmap(int(f.Fd()), 0, fileSize, syscall.PROT_WRITE|syscall.PROT_READ, syscall.MAP_FILE)
+	buff, err := syscall.Mmap(int(f.Fd()), 0, fileSize, syscall.PROT_WRITE|syscall.PROT_READ, syscall.MAP_SHARED)
 	if err != nil {
 		//
 		return nil, err
 	}
-	mf.mappedByteBuff = bytes.NewBuffer(buff)
+
+	mf.dataRef = buff
+	mf.data = (*[defaultMaxFileSize]byte)(unsafe.Pointer(&buff[0]))
 
 	TotalMappedVirtualMemory.Add(int64(fileSize))
 	TotalMappedFile.Inc()
@@ -61,9 +70,46 @@ func NewMmapFile(fileName string, fileSize int) (*MmapFile, error) {
 	return mf, nil
 }
 
-func (f *MmapFile) AppendMessage(data []byte, offset, length int) bool {
+func (f *MmapFile) grow(size int64) error {
+	stat, err := f.file.Stat()
+	if err != nil {
+		//
+		return err
+	}
+	if stat.Size() >= size {
+		return nil
+	}
+	err = f.file.Truncate(size)
+	if err != nil {
+		//
+		return err
+	}
+	return nil
+}
+
+func (f *MmapFile) AppendMessage(data []byte) bool {
 	currPos := int(f.wrotePos.Load())
 
+	length := len(data)
 	if currPos+length <= f.fileSize {
+		err := f.grow(int64(currPos + length))
+		if err != nil {
+			return false
+		}
+		copy(f.data[currPos:], data)
+		f.wrotePos.Add(int32(length))
+		return true
 	}
+	return false
+}
+
+func (f *MmapFile) Close() error {
+	err := syscall.Munmap(f.dataRef)
+	if err != nil {
+		return err
+	}
+
+	f.data = nil
+	f.dataRef = nil
+	return nil
 }
--- server/store/file/default/mmap_file_test.go
+++ server/store/file/default/mmap_file_test.go
@@ -6,8 +6,23 @@ import (
 	"testing"
 )
 
+
+var (
+	fileName = "test.file"
+)
 func TestNewMmapFile(t *testing.T) {
 	fmt.Println("test")
-	f := &MmapFile{}
+	f, err := NewMmapFile(fileName, defaultMemMapSize)
+	assert.Nil(t, err)
 	assert.NotNil(t, f)
+	assert.NotNil(t, f.wrotePos)
+
+	n := 10
+	for i := 0; i < n; i++ {
+		data := []byte(fmt.Sprintf("test test data %d\n", i))
+		f.AppendMessage(data)
+	}
+
+	err = f.Close()
+
 }
